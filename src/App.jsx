2e65441e851bda1a9b89837473ed164674e8bad7
Naveed Wani <waninaveed998@gmail.com>
2023-06-28 01:04:53 +0530

    create signals, classList={} for dynamic content class add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { createSignal } from "solid-js";
 import Card from "./components/Card.jsx";
 function App() {
+    const [darkTheme, setDarkTheme] = createSignal(false);
+    function toggleTheme() {
+        setDarkTheme(!darkTheme())
+    }
     return (
         // single root template
         <div class="container m-auto">
-            <header>
-                <h1 class="">Merch Store</h1>
+            <header
+                class="my-4 p-4 text-xl flex items-center gap-4"
+                classList={{ "bg-blue-900": darkTheme(), "text-white": darkTheme() }}
+            >
+                <span
+                    class="material-symbols-outlined cursor-pointer"
+                    onClick={toggleTheme}
+                >light_mode</span>
             </header>
             <div class="w-full h-24 bg-blue-800">
+                <h1 class="text-white text-7xl">merch store</h1>
             </div>
             <div class="grid grid-cols-4 gap-10 my-4">
                 <Card rounded={true} flat={false} >
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ export default function Card(props) {
         <div class="bg-white p-4 text-center"
             classList={{ "rounded-md": props.rounded, "shadow-md": !props.flat }}
         >
-        {props.children}
+            {props.children}
         </div>
     )
 }
